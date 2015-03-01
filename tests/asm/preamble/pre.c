be4128ec618ac67edaef829149aab50ac9bd8ede
root <root@beaglebone.localdomain>
2015-03-01 21:25:21 +0000

    Built up a test confirming preamble logic

---
+++ tests/asm/preamble/pre.c
@@ -0,0 +1,156 @@
+/******************************************************************************
+* Include Files                                                               *
+******************************************************************************/
+
+// Standard header files
+#include <stdio.h>
+#include <sys/mman.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <unistd.h>
+#include <string.h>
+
+// Driver header file
+#include "prussdrv.h"
+#include <pruss_intc_mapping.h>	 
+
+/******************************************************************************
+* Explicit External Declarations                                              *
+******************************************************************************/
+
+/******************************************************************************
+* Local Macro Declarations                                                    *
+******************************************************************************/
+#define LENGTH 		 1
+#define PRU_NUM 	 0
+#define BUFFER_LENGTH    186
+#define BUFF_BASEADDR    0x90000000
+
+#define HIGH_CODE 	 0xffff
+#define ALT_CODE 	 0xaaaa
+#define LOW_CODE	 0x0000
+
+/******************************************************************************
+* Local Typedef Declarations                                                  *
+******************************************************************************/
+
+
+/******************************************************************************
+* Local Function Declarations                                                 *
+******************************************************************************/
+
+static int LOCAL_exampleInit ( );
+static unsigned short LOCAL_examplePassed ( unsigned short pruNum );
+
+/******************************************************************************
+* Local Variable Definitions                                                  *
+******************************************************************************/
+void *tryByte;
+void *xorResult;
+void *notResult;
+void *addResult;
+unsigned long numBytes = LENGTH * 88;
+
+/******************************************************************************
+* Intertupt Service Routines                                                  *
+******************************************************************************/
+
+
+/******************************************************************************
+* Global Variable Definitions                                                 *
+******************************************************************************/
+
+static int mem_fd;
+static void *ddrMem, *sharedMem;
+static unsigned int *sharedMem_int;
+
+/******************************************************************************
+* Global Function Definitions                                                 *
+******************************************************************************/
+
+int main (void)
+{
+    unsigned int ret0, ret1;
+    tpruss_intc_initdata pruss_intc_initdata = PRUSS_INTC_INITDATA;
+    
+    printf("\nINFO: Starting test.\r\n");
+    /* Initialize the PRU */
+    prussdrv_init ();		
+    
+    /* Open PRU Interrupt */
+    ret0 = prussdrv_open(PRU_EVTOUT_0);
+    if (ret0)
+    {
+        printf("prussdrv_open 0 failed\n");
+        return (ret0);
+    }
+    
+    /* Get the interrupt initialized */
+    prussdrv_pruintc_init(&pruss_intc_initdata);
+
+    /* Initialize example */
+    printf("\tINFO: Initializing test.\r\n");
+    LOCAL_exampleInit();
+    
+    /* Execute example on PRU */
+    printf("\tINFO: Executing test.\r\n");
+    prussdrv_exec_program (0, "./pre.bin");
+
+    /* Wait until PRU0 has finished execution */
+    printf("\tINFO: Waiting for HALT0 command.\r\n");
+    prussdrv_pru_wait_event (PRU_EVTOUT_0);
+    printf("\tINFO: PRU0 completed execution.\r\n");
+    prussdrv_pru_clear_event (PRU0_ARM_INTERRUPT);
+    /* Check if example passed */
+    LOCAL_examplePassed(PRU_NUM);
+    
+    /* Disable PRU and close memory mapping*/
+    prussdrv_pru_disable(0);
+    prussdrv_exit ();
+    munmap(ddrMem, numBytes);
+    close(mem_fd);
+
+    return(0);
+}
+
+/*****************************************************************************
+* Local Function Definitions                                                 *
+*****************************************************************************/
+
+static int LOCAL_exampleInit (  )
+{
+    /* open the device */
+    mem_fd = open("/dev/mem", O_RDWR);
+    if (mem_fd < 0) {
+        printf("Failed to open /dev/mem (%s)\n", strerror(errno));
+        return -1;
+    }	
+    // 0x0FFFFFFF
+    /* map the DDR memory */
+    ddrMem = mmap(0, numBytes,  PROT_WRITE | PROT_READ, MAP_SHARED, mem_fd, BUFF_BASEADDR);
+    if (ddrMem == NULL) {
+        printf("Failed to map the device (%s)\n", strerror(errno));
+        close(mem_fd);
+        return -1;
+    }
+
+    /* Setup required PRU vars in RAM */
+    tryByte    = ddrMem;
+    xorResult  = tryByte + 1;
+    notResult  = xorResult + 1;
+    addResult  = notResult + 1;
+    *((unsigned long*) tryByte) = 0b01100111;
+	
+    return(0);
+}
+static unsigned short LOCAL_examplePassed ( unsigned short pruNum )
+{
+   printf("\n\tAfter PRU Completion:\n");
+   printf("Byte: (%x)\n", (*(unsigned char*) tryByte));
+   printf("XOR: (%x)\n", (*(unsigned char*) xorResult));
+   printf("NOT: (%x)\n", (*(unsigned char*) notResult));
+   printf("ADD: (%x)\n", (*(unsigned char*) addResult));
+
+   return(1);
+}
+
