a9fd9fdcc7f87cec0041e67a73c25482ae3b0a07
root <root@beaglebone.localdomain>
2015-03-01 22:55:45 +0000

    Updated, moved, and clarified all PRU tests. Split into PRU0/PRU1 test components for clarity. Push is from original BBB, where PRU1 is too fucking lazy to do much of anything. About to try to confirm PRU1 operation on BBB2, but outlook not great

--- tests/memory/ddr/pru1/ddrtest.c
+++ tests/memory/ddr/pru1/ddrtest.c
@@ -12,7 +12,7 @@
 
 // Driver header file
 #include "prussdrv.h"
-#include <pruss_intc_mapping.h>
+#include <pruss_intc_mapping.h>	 
 
 /******************************************************************************
 * Explicit External Declarations                                              *
@@ -21,6 +21,14 @@
 /******************************************************************************
 * Local Macro Declarations                                                    *
 ******************************************************************************/
+
+#define PRU_NUM 	 1
+#define CODE1 		 0xabcd
+#define DDR_BASEADDR     0x80000000
+#define OFFSET_DDR	 0x00001000 
+#define OFFSET_SHAREDRAM 2048		//equivalent with 0x00002000
+
+#define PRUSS0_SHARED_DATARAM    4
 
 /******************************************************************************
 * Local Typedef Declarations                                                  *
@@ -31,6 +39,8 @@
 * Local Function Declarations                                                 *
 ******************************************************************************/
 
+static int LOCAL_exampleInit ( );
+static unsigned short LOCAL_examplePassed ( unsigned short pruNum );
 
 /******************************************************************************
 * Local Variable Definitions                                                  *
@@ -46,6 +56,9 @@
 * Global Variable Definitions                                                 *
 ******************************************************************************/
 
+static int mem_fd;
+static void *ddrMem, *sharedMem;
+static unsigned int *sharedMem_int;
 
 /******************************************************************************
 * Global Function Definitions                                                 *
@@ -53,58 +66,91 @@
 
 int main (void)
 {
-    unsigned int ret0;
-    unsigned int ret1;
+    unsigned int ret;
     tpruss_intc_initdata pruss_intc_initdata = PRUSS_INTC_INITDATA;
-
-    printf("\nINFO: Starting %s example.\r\n", "PRU0-1TEST");
+    
+    printf("\nINFO: Starting %s example.\r\n", "PRU_memAccess_DDR_PRUsharedRAM");
     /* Initialize the PRU */
-    prussdrv_init ();
-
+    prussdrv_init ();		
+    
     /* Open PRU Interrupt */
-    ret0 = prussdrv_open(PRU_EVTOUT_0);
-    if (ret0)
+    ret = prussdrv_open(PRU_EVTOUT_1);
+    if (ret)
     {
         printf("prussdrv_open open failed\n");
-		        return (ret0);
+        return (ret);
     }
-    ret1 = prussdrv_open(PRU_EVTOUT_1);
-    if (ret1)
-    {
-        printf("prussdrv_open open failed\n");
-        return (ret1);
-    }
+    
     /* Get the interrupt initialized */
     prussdrv_pruintc_init(&pruss_intc_initdata);
 
+    /* Initialize example */
+    printf("\tINFO: Initializing example.\r\n");
+    LOCAL_exampleInit(PRU_NUM);
+    
     /* Execute example on PRU */
     printf("\tINFO: Executing example.\r\n");
-    prussdrv_exec_program (0, "./pru0test.bin");
-    prussdrv_exec_program (1, "./pru1test.bin");
+    prussdrv_exec_program (PRU_NUM, "./ddrtest.bin");
+
     /* Wait until PRU0 has finished execution */
-    printf("\tINFO: Waiting for HALT0 command.\r\n");
-    prussdrv_pru_wait_event (PRU_EVTOUT_0);
-    printf("\tINFO: PRU0 completed test.\r\n");
-    prussdrv_pru_clear_event (PRU_EVTOUT_0);
-
-    printf("\tINFO: Waiting for HALT1 command.\r\n");
+    printf("\tINFO: Waiting for HALT command.\r\n");
     prussdrv_pru_wait_event (PRU_EVTOUT_1);
-    printf("\tINFO: PRU1 completed test.\r\n");
-    prussdrv_pru_clear_event (PRU_EVTOUT_1);
+    printf("\tINFO: PRU completed transfer.\r\n");
+    prussdrv_pru_clear_event (PRU1_ARM_INTERRUPT);
 
-   /* Disable PRUs and close memory mapping*/
-    prussdrv_pru_disable(0);
-    prussdrv_pru_disable(1);
+    /* Check if example passed */
+    if ( LOCAL_examplePassed(PRU_NUM) )
+    {
+        printf("Example executed succesfully.\r\n");
+    }
+    else
+    {
+        printf("Example failed.\r\n");
+    }
+    
+    /* Disable PRU and close memory mapping*/
+    prussdrv_pru_disable(PRU_NUM); 
     prussdrv_exit ();
+    munmap(ddrMem, 0x0FFFFFFF);
+    close(mem_fd);
 
     return(0);
 }
 
+/*****************************************************************************
+* Local Function Definitions                                                 *
+*****************************************************************************/
 
+static int LOCAL_exampleInit (  )
+{
+    void *DDR_regaddr;
 
+    /* open the device */
+    mem_fd = open("/dev/mem", O_RDWR);
+    if (mem_fd < 0) {
+        printf("Failed to open /dev/mem (%s)\n", strerror(errno));
+        return -1;
+    }	
 
+    /* map the DDR memory */
+    ddrMem = mmap(0, 0x0FFFFFFF, PROT_WRITE | PROT_READ, MAP_SHARED, mem_fd, DDR_BASEADDR);
+    if (ddrMem == NULL) {
+        printf("Failed to map the device (%s)\n", strerror(errno));
+        close(mem_fd);
+        return -1;
+    }
+    
+    /* Store Addends in DDR memory location */
+    DDR_regaddr = ddrMem + OFFSET_DDR;
 
+    *(unsigned long*) DDR_regaddr = CODE1;
 
+    return(0);
+}
 
-
+static unsigned short LOCAL_examplePassed ( unsigned short pruNum )
+{
+    void *DDR_regaddr = ddrMem + OFFSET_DDR;
+    return(*(unsigned long*) DDR_regaddr == 0xabcd);
+}
 
