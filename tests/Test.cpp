b188803143889f476a3b8ea67a20da9fd03adc42
Ben Caine <bcaine0@gmail.com>
2015-09-24 14:03:09 -0400

    Added initial test

---
+++ tests/Test.cpp
@@ -0,0 +1,14 @@
+#include "Transceiver.hpp"
+#include<iostream>
+using namespace std;
+
+void FunctionalTest() {
+  // Put code to do a system wide functional test
+}
+
+int main() {
+
+  FunctionalTest();
+  
+  return 0;
+};
