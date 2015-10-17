db085ff8d16727e0fbabfe7f3d287d4ecc3fcf70
bcaine <bcaine0@gmail.com>
2015-10-17 14:43:54 -0400

    Adding RealtimeControl stub to interface with PRU

---
+++ include/RealtimeControl.hpp
@@ -0,0 +1,31 @@
+/* Module to control PRU (Programmable Realtime Unit) for the Beaglebone Black.
+
+   This should handle initializing the PRUs two units to do data transfer
+   and modulation. Additionally, it should manage the buffer we point
+   the PRU to for access to our encoded/decoded data.
+   
+   Author: Ben Caine
+   Date: October 17, 2015
+*/
+#ifndef REALTIME_HPP
+#define REALTIME_HPP
+
+
+class RealtimeControl {
+
+public:
+  // TODO: This will need location info, 
+  RealtimeControl(int buffer_size);
+  ~RealtimeControl();
+
+  void Write();
+  void Read();
+
+private:
+  bool InitPRU();
+    
+  int _bufsize;
+  int _buf_position;
+  char *_buf;
+
+};
