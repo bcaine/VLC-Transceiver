dcc144a07a21f5f58f03362facdddd30f93ad933
Ryan Nutile <ryan.nutile@gmail.com>
2015-11-10 14:05:21 -0500

    Stripped Down C test, fixed naming for BBB
    prutest.c now contains only code necessary for our testing purposes.
    Might still have some extra includes, but I'm not a perfectionist.
    Renamed all assembly to be easily accessible from the BBB, hopefully all
    future commits will be from there.

--- tests/prutest.c
+++ tests/prutest.c
@@ -22,17 +22,6 @@
 * Local Macro Declarations                                                    *
 ******************************************************************************/
 
-#define PRU_NUM          0
-#define ADDEND1          0x98765400u
-#define ADDEND2          0x12345678u
-#define ADDEND3          0x10210210u
-
-#define DDR_BASEADDR     0x80000000
-#define OFFSET_DDR       0x00001000
-#define OFFSET_SHAREDRAM 2048           //equivalent with 0x00002000
-
-#define PRUSS0_SHARED_DATARAM    4
-
 /******************************************************************************
 * Local Typedef Declarations                                                  *
 ******************************************************************************/
@@ -42,8 +31,6 @@
 * Local Function Declarations                                                 *
 ******************************************************************************/
 
-static int LOCAL_exampleInit ( );
-static unsigned short LOCAL_examplePassed ( unsigned short pruNum );
 
 /******************************************************************************
 * Local Variable Definitions                                                  *
@@ -59,10 +46,6 @@ static unsigned short LOCAL_examplePassed ( unsigned short pruNum );
 * Global Variable Definitions                                                 *
 ******************************************************************************/
 
-static int mem_fd;
-static void *ddrMem, *sharedMem;
-
-static unsigned int *sharedMem_int;
 
 /******************************************************************************
 * Global Function Definitions                                                 *
@@ -70,7 +53,7 @@ static unsigned int *sharedMem_int;
 
 int main (void)
 {
-    unsigned int ret;
+    unsigned int ret0;
     unsigned int ret1;
     tpruss_intc_initdata pruss_intc_initdata = PRUSS_INTC_INITDATA;
 
@@ -79,11 +62,11 @@ int main (void)
     prussdrv_init ();
 
     /* Open PRU Interrupt */
-    ret = prussdrv_open(PRU_EVTOUT_0);
-    if (ret)
+    ret0 = prussdrv_open(PRU_EVTOUT_0);
+    if (ret0)
     {
         printf("prussdrv_open open failed\n");
-		        return (ret);
+		        return (ret0);
     }
     ret1 = prussdrv_open(PRU_EVTOUT_1);
     if (ret1)
@@ -93,10 +76,6 @@ int main (void)
     }
     /* Get the interrupt initialized */
     prussdrv_pruintc_init(&pruss_intc_initdata);
-
-    /* Initialize example */
-    printf("\tINFO: Initializing example.\r\n");
-    LOCAL_exampleInit();
 
     /* Execute example on PRU */
     printf("\tINFO: Executing example.\r\n");
@@ -117,72 +96,6 @@ int main (void)
     prussdrv_pru_disable(0);
     prussdrv_pru_disable(1);
     prussdrv_exit ();
-    munmap(ddrMem, 0x0FFFFFFF);
-    close(mem_fd);
-
-    return(0);
-}
-/*****************************************************************************
-* Local Function Definitions                                                 *
-*****************************************************************************/
-
-static int LOCAL_exampleInit (  )
-{
-    void *DDR_regaddr1, *DDR_regaddr2, *DDR_regaddr3;
-
-    /* open the device */
-    mem_fd = open("/dev/mem", O_RDWR);
-    if (mem_fd < 0) {
-        printf("Failed to open /dev/mem (%s)\n", strerror(errno));
-        return -1;
-    }
-
-    /* map the DDR memory */
-    ddrMem = mmap(0, 0x0FFFFFFF, PROT_WRITE | PROT_READ, MAP_SHARED, mem_fd, DDR_BASEADDR);
-static int LOCAL_exampleInit (  )
-{
-    void *DDR_regaddr1, *DDR_regaddr2, *DDR_regaddr3;
-
-    /* open the device */
-    mem_fd = open("/dev/mem", O_RDWR);
-    if (mem_fd < 0) {
-        printf("Failed to open /dev/mem (%s)\n", strerror(errno));
-        return -1;
-    }
-
-    /* map the DDR memory */
-    ddrMem = mmap(0, 0x0FFFFFFF, PROT_WRITE | PROT_READ, MAP_SHARED, mem_fd, DDR_BASEADDR);
-    if (ddrMem == NULL) {
-        printf("Failed to map the device (%s)\n", strerror(errno));
-        close(mem_fd);
-        return -1;
-    }
-
-    /* Store Addends in DDR memory location */
-    DDR_regaddr1 = ddrMem + OFFSET_DDR;
-    DDR_regaddr2 = ddrMem + OFFSET_DDR + 0x00000004;
-    DDR_regaddr3 = ddrMem + OFFSET_DDR + 0x00000008;
-
-    *(unsigned long*) DDR_regaddr1 = ADDEND1;
-    *(unsigned long*) DDR_regaddr2 = ADDEND2;
-    *(unsigned long*) DDR_regaddr3 = ADDEND3;
-
-	    return(0);
-}
-    if (ddrMem == NULL) {
-        printf("Failed to map the device (%s)\n", strerror(errno));
-        close(mem_fd);
-        return -1;
-    }
-
-    /* Store Addends in DDR memory location */
-    DDR_regaddr1 = ddrMem + OFFSET_DDR;
-    DDR_regaddr2 = ddrMem + OFFSET_DDR + 0x00000004;
-    DDR_regaddr3 = ddrMem + OFFSET_DDR + 0x00000008;
-
-    *(unsigned long*) DDR_regaddr1 = ADDEND1;
-    *(unsigned long*) DDR_regaddr2 = ADDEND2;
-    *(unsigned long*) DDR_regaddr3 = ADDEND3;
 
     return(0);
 }
