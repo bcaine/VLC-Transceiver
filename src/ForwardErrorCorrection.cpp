01dbaaffc8af50892c5f78ba5017537704642b45
Ben Caine <bcaine0@gmail.com>
2016-02-16 16:46:48 -0500

    Removing TODO from FEC cpp file

--- src/ForwardErrorCorrection.cpp
+++ src/ForwardErrorCorrection.cpp
@@ -21,8 +21,6 @@ encoded: bytes of encoded data
 void ForwardErrorCorrection::Encode(unsigned char *data,
                                     unsigned char *encoded,
                                     int len) {
-    // TODO: Make sure len(encoded) == blockcount * 2
-
     // Make sure data size is correct
     int num_bits = len * 8;
     assert(num_bits % 12 == 0);
@@ -47,11 +45,8 @@ data: bytes of our decoded data
 void ForwardErrorCorrection::Decode(unsigned char *encoded,
                                     unsigned char *data,
                                     int len) {
-    // TODO: Make sure len(data) == block_count / 2
-
     // Make sure data size is correct
     int num_bits = len * 8;
-    // assert(num_bits % 23 == 0);
 
     int i = 0, j = 0;
     while (i < num_bits) {
